4418eb38ffd9b63a100f533be9d04e2a80c54654
lmmarise <lmmarise.j@gmail.com>
2021-06-02 23:24:57 +0800

    feat: 增加嵌入式jetty模块

---
+++ embedded-jetty-jsp/src/main/java/org/lmmarise/jetty/demo/Main.java
@@ -0,0 +1,245 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2016 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.lmmarise.jetty.demo;
+
+import org.apache.tomcat.util.scan.StandardJarScanner;
+import org.eclipse.jetty.apache.jsp.JettyJasperInitializer;
+import org.eclipse.jetty.jsp.JettyJspServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.component.AbstractLifeCycle;
+import org.eclipse.jetty.webapp.Configuration;
+import org.lmmarise.jetty.acme.DateServlet;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Example of using JSP's with embedded jetty and using a
+ * lighter-weight ServletContextHandler instead of a WebAppContext. 
+ * 
+ * This example is somewhat odd in that it uses custom tag libs which reside
+ * in a WEB-INF directory, even though WEB-INF is not meaningful to
+ * a ServletContextHandler. This just shows that once we have
+ * properly initialized the jsp engine, you can even use this type of
+ * custom taglib, even if you don't have a full-fledged webapp.
+ * 
+ */
+public class Main
+{
+    static
+    {
+       LoggingUtil.config();
+    }
+
+    // Resource path pointing to where the WEBROOT is
+    private static final String WEBROOT_INDEX = "/webroot/";
+    
+    /**
+     * JspStarter for embedded ServletContextHandlers
+     * 
+     * This is added as a bean that is a jetty LifeCycle on the ServletContextHandler.
+     * This bean's doStart method will be called as the ServletContextHandler starts,
+     * and will call the ServletContainerInitializer for the jsp engine.
+     *
+     */
+    public static class JspStarter extends AbstractLifeCycle implements ServletContextHandler.ServletContainerInitializerCaller
+    {
+        JettyJasperInitializer sci;
+        ServletContextHandler context;
+        
+        public JspStarter (ServletContextHandler context)
+        {
+            this.sci = new JettyJasperInitializer();
+            this.context = context;
+            this.context.setAttribute("org.apache.tomcat.JarScanner", new StandardJarScanner());
+        }
+
+        @Override
+        protected void doStart() throws Exception
+        {
+            ClassLoader old = Thread.currentThread().getContextClassLoader();
+            Thread.currentThread().setContextClassLoader(context.getClassLoader());
+            try
+            {
+                sci.onStartup(null, context.getServletContext());   
+                super.doStart();
+            }
+            finally
+            {
+                Thread.currentThread().setContextClassLoader(old);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        int port = 8080;
+
+        Main main = new Main(port);
+        main.start();
+        main.waitForInterrupt();
+    }
+
+    private static final Logger LOG = Logger.getLogger(Main.class.getName());
+
+    private int port;
+    private Server server;
+
+    public Main(int port)
+    {
+        this.port = port;
+    }
+
+    public void start() throws Exception
+    {
+        server = new Server();
+        
+        // Define ServerConnector
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(port);
+        server.addConnector(connector);
+    
+        // Add annotation scanning (for WebAppContexts)
+        Configuration.ClassList classlist = Configuration.ClassList
+                .setServerDefault( server );
+        classlist.addBefore(
+                "org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
+                "org.eclipse.jetty.annotations.AnnotationConfiguration" );
+
+        // Base URI for servlet context
+        URI baseUri = getWebRootResourceUri();
+        LOG.info("Base URI: " + baseUri);
+        
+        // Create Servlet context
+        ServletContextHandler servletContextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        servletContextHandler.setContextPath("/");
+        servletContextHandler.setResourceBase(baseUri.toASCIIString());
+
+        // Since this is a ServletContextHandler we must manually configure JSP support.
+        enableEmbeddedJspSupport(servletContextHandler);
+    
+        // Add Application Servlets
+        servletContextHandler.addServlet(DateServlet.class, "/date/");
+        // Create Example of mapping jsp to path spec
+        ServletHolder holderAltMapping = new ServletHolder();
+        holderAltMapping.setName("foo.jsp");
+        holderAltMapping.setForcedPath("/test/foo/foo.jsp");
+        servletContextHandler.addServlet(holderAltMapping, "/test/foo/");
+    
+        // Default Servlet (always last, always named "default")
+        ServletHolder holderDefault = new ServletHolder("default", DefaultServlet.class);
+        holderDefault.setInitParameter("resourceBase", baseUri.toASCIIString());
+        holderDefault.setInitParameter("dirAllowed", "true");
+        servletContextHandler.addServlet(holderDefault, "/");
+        server.setHandler(servletContextHandler);
+
+        // Start Server
+        server.start();
+
+        // Show server state
+        if (LOG.isLoggable(Level.FINE))
+        {
+            LOG.fine(server.dump());
+        }
+    }
+    
+    /**
+     * Setup JSP Support for ServletContextHandlers.
+     * <p>
+     *   NOTE: This is not required or appropriate if using a WebAppContext.
+     * </p>
+     *
+     * @param servletContextHandler the ServletContextHandler to configure
+     * @throws IOException if unable to configure
+     */
+    private void enableEmbeddedJspSupport(ServletContextHandler servletContextHandler) throws IOException
+    {
+        // Establish Scratch directory for the servlet context (used by JSP compilation)
+        File tempDir = new File(System.getProperty("java.io.tmpdir"));
+        File scratchDir = new File(tempDir.toString(), "embedded-jetty-jsp");
+    
+        if (!scratchDir.exists())
+        {
+            if (!scratchDir.mkdirs())
+            {
+                throw new IOException("Unable to create scratch directory: " + scratchDir);
+            }
+        }
+        servletContextHandler.setAttribute("javax.servlet.context.tempdir", scratchDir);
+    
+        // Set Classloader of Context to be sane (needed for JSTL)
+        // JSP requires a non-System classloader, this simply wraps the
+        // embedded System classloader in a way that makes it suitable
+        // for JSP to use
+        ClassLoader jspClassLoader = new URLClassLoader(new URL[0], this.getClass().getClassLoader());
+        servletContextHandler.setClassLoader(jspClassLoader);
+        
+        // Manually call JettyJasperInitializer on context startup
+        servletContextHandler.addBean(new JspStarter(servletContextHandler));
+        
+        // Create / Register JSP Servlet (must be named "jsp" per spec)
+        ServletHolder holderJsp = new ServletHolder("jsp", JettyJspServlet.class);
+        holderJsp.setInitOrder(0);
+        holderJsp.setInitParameter("logVerbosityLevel", "DEBUG");
+        holderJsp.setInitParameter("fork", "false");
+        holderJsp.setInitParameter("xpoweredBy", "false");
+        holderJsp.setInitParameter("compilerTargetVM", "1.8");
+        holderJsp.setInitParameter("compilerSourceVM", "1.8");
+        holderJsp.setInitParameter("keepgenerated", "true");
+        servletContextHandler.addServlet(holderJsp, "*.jsp");
+    }
+    
+    private URI getWebRootResourceUri() throws FileNotFoundException, URISyntaxException
+    {
+        URL indexUri = this.getClass().getResource(WEBROOT_INDEX);
+        if (indexUri == null)
+        {
+            throw new FileNotFoundException("Unable to find resource " + WEBROOT_INDEX);
+        }
+        // Points to wherever /webroot/ (the resource) is
+        return indexUri.toURI();
+    }
+
+    public void stop() throws Exception
+    {
+        server.stop();
+    }
+
+    /**
+     * Cause server to keep running until it receives a Interrupt.
+     * <p>
+     * Interrupt Signal, or SIGINT (Unix Signal), is typically seen as a result of a kill -TERM {pid} or Ctrl+C
+     * @throws InterruptedException if interrupted
+     */
+    public void waitForInterrupt() throws InterruptedException
+    {
+        server.join();
+    }
+}
---
+++ nio/src/main/java/org/lmmarise/nio/Main.java
@@ -0,0 +1,43 @@
+package org.lmmarise.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+
+/**
+ * @author lmmarise.j@gmail.com
+ * @date 2021/6/2 9:13 下午
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // 服务端channel
+        ServerSocketChannel server = ServerSocketChannel.open();
+        server.bind(new InetSocketAddress(8088));
+        server.configureBlocking(false);
+        // 注册channel感兴趣的连接事件
+        Selector selector = Selector.open();
+        server.register(selector, SelectionKey.OP_ACCEPT);  // 连接事件
+        // 建立连接处理事件
+        while (true) {
+            selector.select();  // 查询IO事件
+            Iterator<SelectionKey> i = selector.selectedKeys().iterator();
+            while (i.hasNext()) {
+                SelectionKey key = i.next();
+                i.remove();
+
+                if (key.isAcceptable()) {
+                    SocketChannel client = server.accept();     // 建立一个新连接
+                    client.configureBlocking(false);
+
+                    client.register(selector, SelectionKey.OP_READ);    // 告诉Selector现在对IO可读感兴趣
+                }
+            }
+        }
+    }
+
+}
